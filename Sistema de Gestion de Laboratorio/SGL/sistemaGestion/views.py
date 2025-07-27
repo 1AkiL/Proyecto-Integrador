9029aff29c619ba966bc97fd27f7fd66d70ae4cc
1AkiL <152743719+1AkiL@users.noreply.github.com>
2025-07-27 15:29:30 -0700

    It's little
    I hope tomorrow is a better day for the project, though we're going to get murdered

--- Sistema de Gestion de Laboratorio/SGL/sistemaGestion/views.py
+++ Sistema de Gestion de Laboratorio/SGL/sistemaGestion/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Recurso, Laboratorio
+from .models import Recurso, Laboratorio, Usuario
+from django.utils.timezone import localtime, localdate
 
 # Create your views here.
 #TODO Make the login system work
